c7d78fd2e301d7244204f29fdb8f677ab35a07f3
Alvaro Castro <moltar89@gmail.com>
2018-03-09 15:43:26 -0300

    Import validate.js from node_modules to vendor

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict';
 
+var path = require('path'),
+	MergeTrees = require('broccoli-merge-trees'),
+	Funnel = require('broccoli-funnel'),
+	UnwatchedDir = require('broccoli-source').UnwatchedDir;
+
 module.exports = {
-  name: 'ember-validate'
+	name: 'ember-validate'
+
+	isDevelopingAddon: function () {
+		return true;
+	},
+
+	included: function (app) {
+		this._super.included(app);
+
+		app.import('vendor/validate.js');
+		app.import('vendor/shims/validate.js');
+	},
+
+	treeForVendor: function (tree) {
+		var dir = new UnwatchedDir(path.dirname(require.resolve('validate.js/validate.js'))),
+			validatejsTree = new Funnel(dir, {
+				files: ['validate.js'],
+			});
+
+		return new MergeTrees([tree, validatejsTree]);
+	}
 };
